16ee6891b5c7378428f0f3e079e6f3b2443ed4c9
Gym <gym_zhang@126.com>
2017-02-16 09:46:01 +0800

    add Appbar material design effect.

--- app/src/main/java/com/anddle/weatherapp/MainActivity.java
+++ app/src/main/java/com/anddle/weatherapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.anddle.weatherapp;
 
 import android.os.AsyncTask;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -26,54 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private List<WeatherMoreInfo> mWeatherMoreInfoList;
     private UpdateTask mUpdateTask;
 
-    private final String FAKE_DATA= "{\n" +
-            "    \"error_code\": \"0\",\n" +
-            "    \"data\": {\n" +
-            "        \"location\": \"成都\",\n" +
-            "        \"temperature\": \"23°\",\n" +
-            "        \"temperature_range\": \"18℃~23℃\",\n" +
-            "        \"weather_code\": \"5\",\n" +
-            "        \"wind_direction\": \"东南\",\n" +
-            "        \"wind_level\": \"1级\",\n" +
-            "        \"humidity_level\": \"30%\",\n" +
-            "        \"air_quality\": \"良\",\n" +
-            "        \"sport_level\": \"适宜\",\n" +
-            "        \"ultraviolet_ray\": \"弱\",\n" +
-            "        \"forcast\": [\n" +
-            "            {\n" +
-            "                \"date\": \"明天\",\n" +
-            "                \"temperature_range\": \"18℃~23℃\",\n" +
-            "                \"weather_code\": \"0\"\n" +
-            "            },\n" +
-            "            {\n" +
-            "                \"date\": \"星期六\",\n" +
-            "                \"temperature_range\": \"17℃~21℃\",\n" +
-            "                \"weather_code\": \"1\"\n" +
-            "            },\n" +
-            "            {\n" +
-            "                \"date\": \"星期日\",\n" +
-            "                \"temperature_range\": \"19℃~24℃\",\n" +
-            "                \"weather_code\": \"3\"\n" +
-            "            },\n" +
-            "            {\n" +
-            "                \"date\": \"星期一\",\n" +
-            "                \"temperature_range\": \"16℃~22℃\",\n" +
-            "                \"weather_code\": \"4\"\n" +
-            "            },\n" +
-            "            {\n" +
-            "                \"date\": \"星期二\",\n" +
-            "                \"temperature_range\": \"20℃~26℃\",\n" +
-            "                \"weather_code\": \"2\"\n" +
-            "            }\n" +
-            "        ]\n" +
-            "    }\n" +
-            "}";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("TEST", "Weather app launched");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
 
         mWeatherMoreInfoListView = (ListView) findViewById(R.id.weather_more_info_list);
         mWeatherMoreInfoList = new ArrayList<>();
@@ -228,17 +190,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private void updateWeatherDetail(String location, String temperature, String temperatureRange, int weatherCode) {
 
         TextView currentTemperatureView = (TextView) findViewById(R.id.current_temperature);
         TextView temperatureRangeView = (TextView) findViewById(R.id.temperature_range);
         ImageView weatherIcon = (ImageView) findViewById(R.id.weather_icon);
-        TextView weatherLocation = (TextView) findViewById(R.id.weather_location);
 
         currentTemperatureView.setText(temperature);
         temperatureRangeView.setText(temperatureRange);
-        weatherLocation.setText(location);
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_layout);
+        collapsingToolbar.setTitle(location);
 
         int iconId = R.mipmap.ic_sunny_cloudy_l;
         switch (weatherCode) {
